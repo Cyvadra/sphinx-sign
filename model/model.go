cf59345fa37bb796b58dd7534043fccd7a622c44
邓捷 <dj@lifesdk.com>
2021-11-01 17:27:53 +0800

    Initialize model

---
+++ model/model.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type KeyStore struct {
+	ID            uint64 `gorm:"primaryKey"`
+	CreatetimeUtc int64  `gorm:"autoCreateTime"`
+	Salt          string `gorm:"size:64"`
+	Address       string `gorm:"size:80;uniqueIndex"`
+	EncryptedKey  string `gorm:"size:255;comment:'v0 raw'"`
+}
+
+type Secret2FA struct {
+	ID        uint64 `gorm:"primaryKey"`
+	UUID      string `gorm:"size:64"`
+	Address   string `gorm:"size:80;uniqueIndex"`
+	Text      string `gorm:"size:32"`
+	CreatedAt uint64 `gorm:"autoCreateTime"`
+	CheckedAt uint64 `gorm:"autoUpdateTime"`
+	DeletedAt uint64 `gorm:"default:null"`
+}
