c86d69e5023a92332dd734ae695af4075381bb41
Cyvadra <dj@lifesdk.com>
2021-11-06 23:30:25 +0800

    bugfix try

--- tmp.go
+++ tmp.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/myxtype/filecoin-client"
 	"github.com/myxtype/filecoin-client/local"
 	"github.com/myxtype/filecoin-client/types"
@@ -18,32 +18,41 @@ var Client *filecoin.Client
 
 func main() {
 	var err error
-	to_addr := "t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra"
+	toAddr := "t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra"
+	pkStr := "c3pS5JcZEM1C5Yukor63mQ8DvADh1qQN/GrUsRA20XE="
+	var pk []byte
+	_, err = base64.StdEncoding.Decode([]byte(pkStr), pk)
+	if err != nil {
+		fmt.Println("pk解码失败")
+	}
 	// 设置主机
 	address.CurrentNetwork = address.Mainnet
 	SetHostWithToken("172.16.30.117", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.ppK_nggwygh6kCPDlktdBtkGaqQXxoXM99iNx3-tZ9E")
 	// 设置key
-	pk, err := hex.DecodeString("7b2254797065223a22736563703235366b31222c22507269766174654b6579223a223772503034624643507854562b356a6f6954644b76366d2f61763064335a716c304a757879577856346e493d227d")
 	ki := types.KeyInfo{
-		Type:       "secp256k1",
+		Type:       types.KTBLS,
 		PrivateKey: pk,
 	}
 	// 由key生成并确认地址
-	addr, err := local.WalletPrivateToAddress(crypto.SigTypeBLS, pk)
+	// addr, err := local.WalletPrivateToAddress(crypto.SigTypeBLS, pk)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	// Debug output
-	fmt.Printf("key: %v\n", hex.EncodeToString(ki.PrivateKey))
-	fmt.Printf("address: %v\n", addr.String())
+	fmt.Println("key: ", hex.EncodeToString(ki.PrivateKey))
+	// fmt.Printf("address: %v\n", addr.String())
+	// addr should be t3vciz4skae3yyfnsm54rjri3ubhbk4vklxmdvykwosnqhikehocf26rfiegekwa743ypem4oiv3uq7wuxwcca
+	addr, err := address.NewFromString("t3vciz4skae3yyfnsm54rjri3ubhbk4vklxmdvykwosnqhikehocf26rfiegekwa743ypem4oiv3uq7wuxwcca")
 
-	to, err := address.NewFromString(to_addr)
+	to, err := address.NewFromString(toAddr)
 
 	// 转移0.001FIL到目标地址
 	msg := &types.Message{
-		Version:    0,
-		To:         to,
-		From:       *addr,
+		Version: 0,
+		To:      to,
+		// From:       *addr,
+		From:       addr,
 		Nonce:      0,
 		Value:      filecoin.FromFil(decimal.NewFromFloat(0.001)),
 		GasLimit:   0,
@@ -63,8 +72,10 @@ func main() {
 	//}
 
 	// 离线签名
-	s, err := local.WalletSignMessage(types.KTBLS, ki.PrivateKey, msg)
+	s, err := local.WalletSignMessage(types.KTSecp256k1, ki.PrivateKey, msg)
 	if err != nil {
+		fmt.Println("离线签名失败")
+		fmt.Println(s)
 		fmt.Println(err)
 		return
 	} else {
