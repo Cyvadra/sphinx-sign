d4a501b358196865174a785af73a844c749b76ec
Cyvadra <dj@lifesdk.com>
2021-11-06 21:45:24 +0800

    debug signing algo; MARK: need to store pk and it's algo

--- agent/fil/fil.go
+++ agent/fil/fil.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/myxtype/filecoin-client"
 	"github.com/myxtype/filecoin-client/local"
 	"github.com/myxtype/filecoin-client/types"
@@ -16,25 +17,28 @@ import (
 var Client *filecoin.Client
 
 func main() {
-
 	var err error
-
+	to_addr := "t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra"
+	// 设置主机
 	SetHostWithToken("172.16.30.117", "")
+	// 设置🔑
+	pk := []byte("7b2254797065223a22736563703235366b31222c22507269766174654b6579223a223772503034624643507854562b356a6f6954644b76366d2f61763064335a716c304a757879577856346e493d227d")
+	ki := types.KeyInfo{
+		Type:       "secp256k1",
+		PrivateKey: pk,
+	}
+	addr, err := local.WalletPrivateToAddress(crypto.SigTypeSecp256k1, pk)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// 设置网络类型
 	address.CurrentNetwork = address.Mainnet
 
-	// 生产新的地址
-	// 新地址有转入fil才激活，不然没法用
-	ki, addr, err := local.WalletNew(types.KTSecp256k1)
-	if err != nil {
-		panic(err)
-	}
-
 	fmt.Printf("key: %v\n", hex.EncodeToString(ki.PrivateKey))
 	fmt.Printf("address: %v\n", addr.String())
 
-	to, err := address.NewFromString("t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra")
+	to, err := address.NewFromString(to_addr)
 
 	// 转移0.001FIL到f1yfi4yslez2hz3ori5grvv3xdo3xkibc4v6xjusy
 	msg := &types.Message{
@@ -64,10 +68,12 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		return
+	} else {
+		fmt.Println("signed message: ")
+		fmt.Println(s)
 	}
 
 	println(hex.EncodeToString(s.Signature.Data))
-	// 47bcbb167fd9040bd02dba02789bc7bc0463c290db1be9b07065c12a64fb84dc546bef7aedfba789d0d7ce2c4532f8fa0d2dd998985ad3ec1a8b064c26e4625a01
 
 	// 验证签名
 	if err := local.WalletVerifyMessage(s); err != nil {
