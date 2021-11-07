c8ed208802303573052cfa5c885d1fc7103e72c6
Cyvadra <dj@lifesdk.com>
2021-11-07 18:00:18 +0800

    'tmp.go' run pass; todo: extract into modules

--- go.mod
+++ go.mod
@@ -20,24 +20,25 @@ require (
 	github.com/ipfs/go-cid v0.1.0 // indirect
 	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
 	github.com/ipfs/go-ipld-cbor v0.0.5 // indirect
-	github.com/ipfs/go-ipld-format v0.0.1 // indirect
+	github.com/ipfs/go-ipld-format v0.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
-	github.com/klauspost/cpuid/v2 v2.0.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
 	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/multiformats/go-base32 v0.0.3 // indirect
+	github.com/multiformats/go-base32 v0.0.4 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
 	github.com/multiformats/go-multibase v0.0.3 // indirect
-	github.com/multiformats/go-multihash v0.0.15 // indirect
+	github.com/multiformats/go-multihash v0.0.16 // indirect
 	github.com/multiformats/go-varint v0.0.6 // indirect
-	github.com/polydawn/refmt v0.0.0-20190807091052-3d65705ee9f1 // indirect
+	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e // indirect
+	github.com/supranational/blst v0.3.6 // indirect
 	github.com/whyrusleeping/cbor-gen v0.0.0-20210713220151-be142a5ae1a8 // indirect
-	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20211106132015-ebca88c72f68 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- tmp.go
+++ tmp.go
@@ -17,64 +17,19 @@ import (
 
 var Client *filecoin.Client
 
-func main() {
-	var err error
-	toAddr := "t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra"
-	pkStr := "c3pS5JcZEM1C5Yukor63mQ8DvADh1qQN/GrUsRA20XE="
-	var pk []byte
-	_, err = base64.StdEncoding.Decode([]byte(pkStr), pk)
-	if err != nil {
-		fmt.Println("pk解码失败")
-	}
-	// 设置主机
-	address.CurrentNetwork = address.Mainnet
-	SetHostWithToken("172.16.30.117", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.ppK_nggwygh6kCPDlktdBtkGaqQXxoXM99iNx3-tZ9E")
-	// 设置key
-	ki := types.KeyInfo{
-		Type:       types.KTBLS,
-		PrivateKey: pk,
-	}
+type SignInfoFIL struct {
+	Nonce      int
+	Version    int
+	GasLimit   int // filecoin.FromFil(decimal.NewFromFloat(0.0001))
+	GasFeeCap  int
+	GasPremium int
+	Method     int
+	Value      int
+}
 
-	// 测试
-	fmt.Println("===============")
-	fmt.Println(crypto.SigTypeSecp256k1)
-	fmt.Println(crypto.SigTypeBLS)
-	// 由key生成并确认地址
-	fromAddr, err := local.WalletPrivateToAddress(crypto.SigTypeBLS, pk)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// Debug output
-	fmt.Println("key: ", hex.EncodeToString(ki.PrivateKey))
-	fmt.Printf("address: %v\n", fromAddr.String())
-	fmt.Println("expected address: t3vciz4skae3yyfnsm54rjri3ubhbk4vklxmdvykwosnqhikehocf26rfiegekwa743ypem4oiv3uq7wuxwcca")
-	to, err := address.NewFromString(toAddr)
-	// 转移0.001FIL到目标地址
-	msg := &types.Message{
-		Version:    0,
-		To:         to,
-		From:       *fromAddr,
-		Nonce:      0,
-		Value:      filecoin.FromFil(decimal.NewFromFloat(0.001)),
-		GasLimit:   0,
-		GasFeeCap:  abi.NewTokenAmount(10000),
-		GasPremium: abi.NewTokenAmount(10000),
-		Method:     0,
-		Params:     nil,
-	}
-
-	// 最大手续费0.0001 FIL
-	// maxFee := filecoin.FromFil(decimal.NewFromFloat(0.0001))
-
-	// 估算GasLimit
-	//msg, err = client.GasEstimateMessageGas(context.Background(), msg, &types.MessageSendSpec{MaxFee: maxFee}, nil)
-	//if err != nil {
-	//	panic(err)
-	//}
-
+func OfflineSign(ki *types.KeyInfo, msg *types.Message) (s *types.SignedMessage, err error) {
 	// 离线签名
-	s, err := local.WalletSignMessage(types.KTSecp256k1, ki.PrivateKey, msg)
+	s, err = local.WalletSignMessage(types.KTBLS, ki.PrivateKey, msg)
 	if err != nil {
 		fmt.Println("离线签名失败")
 		fmt.Println(s)
@@ -84,24 +39,25 @@ func main() {
 		fmt.Println("signed message: ")
 		fmt.Println(s)
 	}
-
 	println(hex.EncodeToString(s.Signature.Data))
-
 	// 验证签名
-	if err := local.WalletVerifyMessage(s); err != nil {
-		fmt.Println("验证签名失败")
-		fmt.Println(err)
-		return
+	if err = local.WalletVerifyMessage(s); err != nil {
+		fmt.Println("验证签名失败", err)
 	}
+	return
+}
 
+func BroadcastMessage(s *types.SignedMessage) (err error) {
+	// 消息广播
 	mid, err := Client.MpoolPush(context.Background(), s)
 	if err != nil {
 		fmt.Println("消息广播失败")
 		fmt.Println(err)
-		return
+	} else {
+		fmt.Println("消息发送成功，message id:")
+		fmt.Println(mid.String())
 	}
-	println("消息发送成功，message id:")
-	println(mid.String())
+	return
 }
 
 func lotusURL(host string) string {
@@ -110,4 +66,69 @@ func lotusURL(host string) string {
 
 func SetHostWithToken(str, token string) {
 	Client = filecoin.NewClient(lotusURL(str), token)
+}
+
+func main() {
+	var err error
+	// 静态设置
+	toAddr, err := address.NewFromString("t1gvkap5jmv5k7gwpa42zj43i2oaai5zg74n66xra")
+	pkStr := "c3pS5JcZEM1C5Yukor63mQ8DvADh1qQN/GrUsRA20XE="
+	if err != nil {
+		fmt.Println("收款地址错误", err)
+		return
+	}
+	var fromAddr *address.Address
+	var pk []byte
+	pk, err = base64.StdEncoding.DecodeString(pkStr)
+	if err != nil {
+		fmt.Println("pk解码失败", err)
+		return
+	}
+	// 设置key
+	ki := &types.KeyInfo{
+		Type:       types.KTBLS,
+		PrivateKey: pk,
+	}
+	// 由key生成并确认地址
+	fromAddr, err = local.WalletPrivateToAddress(crypto.SigTypeBLS, ki.PrivateKey)
+	if err != nil {
+		fmt.Println("生成地址失败", err)
+		return
+	}
+	// 设置主机
+	address.CurrentNetwork = address.Mainnet
+	SetHostWithToken("172.16.30.117", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.ppK_nggwygh6kCPDlktdBtkGaqQXxoXM99iNx3-tZ9E")
+	// 在此编辑message
+	// 需要获取Nonce
+	msg := &types.Message{
+		Version:    0,
+		To:         toAddr,
+		From:       *fromAddr,
+		Nonce:      14,
+		Value:      filecoin.FromFil(decimal.NewFromFloat(123.456)),
+		GasLimit:   0,
+		GasFeeCap:  abi.NewTokenAmount(100),
+		GasPremium: abi.NewTokenAmount(100),
+		Method:     0,
+		Params:     nil,
+	}
+	// 设置最大手续费
+	maxFee := filecoin.FromFil(decimal.NewFromFloat(0.0001))
+	// 估算GasLimit
+	msg, err = Client.GasEstimateMessageGas(context.Background(), msg, &types.MessageSendSpec{MaxFee: maxFee}, nil)
+	if err != nil {
+		fmt.Println("GasEstimateMessageGas错误", err)
+		return
+	}
+	signedMsg, err := OfflineSign(ki, msg)
+	if err != nil {
+		fmt.Println("签名失败", err)
+		return
+	}
+	err = BroadcastMessage(signedMsg)
+	if err != nil {
+		fmt.Println("广播失败", err)
+		return
+	}
+	return
 }
